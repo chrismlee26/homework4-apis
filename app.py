9264aa10134b9072c16fe3fe3f6ae70f4193757c
CHRIS_LEE <chrismlee87@gmail.com>
2020-11-20 23:31:23 -0800

    master - working p1

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ from geopy.geocoders import Nominatim
 ################################################################################
 
 app = Flask(__name__)
-
-# Get the API key from the '.env' file
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
@@ -41,40 +39,47 @@ def get_letter_for_units(units):
 @app.route('/results')
 def results():
     """Displays results for current weather conditions."""
-    # TODO: Use 'request.args' to retrieve the city & units from the query
-    # parameters.
-    city = ''
-    units = ''
+    q = request.args.get('city')
+    units = request.args.get('units')
+
+
 
     url = 'http://api.openweathermap.org/data/2.5/weather'
-    params = {
-        # TODO: Enter query parameters here for the 'appid' (your api key),
-        # the city, and the units (metric or imperial).
-        # See the documentation here: https://openweathermap.org/current
 
+    params = {
+        'q' : q,
+        'units' : units,
+        'appid' : API_KEY
     }
 
     result_json = requests.get(url, params=params).json()
 
     # Uncomment the line below to see the results of the API call!
-    # pp.pprint(result_json)
+    pp.pprint(result_json)
 
-    # TODO: Replace the empty variables below with their appropriate values.
-    # You'll need to retrieve these from the result_json object above.
+    city = result_json['name']
+    description = result_json['weather'][0]['description']
+    temp = result_json['main']['temp']
+    humidity = result_json['main']['humidity']
+    wind_speed = result_json['wind']['speed']
+    sunrise = result_json['sys']['sunrise']
+    sunset = result_json['sys']['sunset']
+    units_letter = get_letter_for_units(units)
+    date = datetime.now(tz=None)
 
     # For the sunrise & sunset variables, I would recommend to turn them into
     # datetime objects. You can do so using the `datetime.fromtimestamp()` 
     # function.
     context = {
-        'date': datetime.now(),
-        'city': '',
-        'description': '',
-        'temp': '',
-        'humidity': '',
-        'wind_speed': '',
-        'sunrise': '',
-        'sunset': '',
-        'units_letter': get_letter_for_units(units)
+        'date' : date,
+        'city' : city,
+        'description' : description,
+        'temp' : temp,
+        'humidity' : humidity,
+        'wind_speed' : wind_speed,
+        'sunrise' : sunrise,
+        'sunset' : sunset,
+        'units_letter' : units_letter
     }
 
     return render_template('results.html', **context)
